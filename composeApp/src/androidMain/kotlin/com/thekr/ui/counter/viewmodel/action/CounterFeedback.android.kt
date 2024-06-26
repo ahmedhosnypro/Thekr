3618c93d675328d3ffd5c37cd8b053184760f87d
Ahmed Hosny <ahhosnyas@gmail.com>
2024-06-26 17:40:51 +0300

    Add CounterFeedback and update PlaySound to support multiplatform sound and vibration.

---
+++ composeApp/src/androidMain/kotlin/com/thekr/ui/counter/viewmodel/action/CounterFeedback.android.kt
@@ -0,0 +1,56 @@
+package com.thekr.ui.counter.viewmodel.action
+
+import android.content.Context
+import android.content.Context.VIBRATOR_SERVICE
+import android.os.Build
+import android.os.Vibrator
+import android.os.VibratorManager
+import com.thekr.ThekrApplication.Companion.appContext
+//import com.thekr.ui.util.TTSSpeaker.textToSpeech
+
+actual fun vibrate(){
+    vibrator().vibrate(longArrayOf(0, 16), -1)
+}
+
+fun vibrator() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+    val vibratorManager =
+        appContext.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+    vibratorManager.defaultVibrator
+} else {
+    @Suppress("DEPRECATION")
+    appContext.getSystemService(VIBRATOR_SERVICE) as Vibrator
+}
+
+//suspend fun alertSleep(
+//    player: ExoPlayer?,
+//    context: Context,
+//    soundFileName: String?,
+//) {
+//    if (soundFileName != null) {
+//        playSound(player, context, soundFileName)
+//    } else {
+//        playSound(player, context, SoundResourceHelper.getRandomResourceId())
+//    }
+//    val vibrator = vibrator(context)
+//
+//    val customizableCrescendo = mutableListOf<Long>()
+//
+//    var onTime = 100
+//    var offTime = 1500
+//
+//    repeat(10) {
+//        customizableCrescendo.add(onTime.toLong())
+//        customizableCrescendo.add(offTime.toLong())
+//
+//        onTime += 50
+//        offTime -= 50
+//    }
+//
+//    val crescendoPattern = customizableCrescendo.toLongArray()
+//    patternList.add(crescendoPattern)
+//    patternList.forEach {
+//        @Suppress("DEPRECATION")
+//        vibrator.vibrate(it, -1)
+//        delay(1000)
+//    }
+//}
--- composeApp/src/androidMain/kotlin/com/thekr/ui/counter/viewmodel/action/PlaySound.android.kt
+++ composeApp/src/androidMain/kotlin/com/thekr/ui/counter/viewmodel/action/PlaySound.android.kt
@@ -1,15 +1,20 @@
 package com.thekr.ui.counter.viewmodel.action
 
 import com.thekr.ThekrApplication.Companion.appContext
-import com.thekr.ui.counter.viewmodel.action.ThekrSoundPlayer.playerSoundChannel
 import com.thekr.ui.counter.viewmodel.action.ThekrSoundPlayer.scope
+import korlibs.audio.sound.Sound
 import korlibs.audio.sound.SoundAudioStream
 import korlibs.io.android.withAndroidContext
 
-actual suspend fun SoundAudioStream.platformPlay() = withAndroidContext(appContext) {
-    val channel = play()
-    playerSoundChannel = channel
-    channel.onCompleted(scope.coroutineContext) {
-        playerSoundChannel = null
+actual suspend fun SoundAudioStream.platformPlay(player: SoundPlayer) =
+    withAndroidContext(appContext) {
+        val channel = play()
+        player.soundChannel = channel
+        channel.onCompleted(scope.coroutineContext) {
+            player.soundChannel = null
+        }
     }
+
+actual suspend fun Sound.platformPlay() = withAndroidContext(appContext) {
+    play()
 }
