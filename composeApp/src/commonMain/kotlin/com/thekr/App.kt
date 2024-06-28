cf1bef2e0636ddfb7379b963752ac265c33f8431
Ahmed Hosny <ahhosnyas@gmail.com>
2024-06-28 19:00:01 +0300

    delete BackgroundImageShowCase.kt

--- composeApp/src/commonMain/kotlin/com/thekr/App.kt
+++ composeApp/src/commonMain/kotlin/com/thekr/App.kt
@@ -15,15 +15,14 @@ import com.thekr.resources.*
 import com.thekr.theme.AppTheme
 import com.thekr.theme.LocalThemeIsDark
 import com.thekr.ui.CounterApp
-import com.thekr.ui.modifier.BackgroundImageShowCase
 import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
 
 @Composable
 internal fun App() = AppTheme {
-//    CounterApp()
-    BackgroundImageShowCase()
+    CounterApp()
+//    BackgroundImageShowCase()
 }
 
 @Composable
