d30115f0efd66e582354ee2123b6b465dd5a46cc
ahmedhosnypro <ahhosnyas@gmail.com>
2025-02-25 04:28:46 +0200

    Refactor ZekrBar component to simplify layout and remove unused imports

--- composeApp/src/commonMain/kotlin/com/thekr/ui/bar/top/ZekrBar.kt
+++ composeApp/src/commonMain/kotlin/com/thekr/ui/bar/top/ZekrBar.kt
@@ -1,26 +1,14 @@
 package com.thekr.ui.bar.top
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -29,7 +17,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.thekr.data.proto.ThemeMode
 import com.thekr.data.settings.SettingsDetails
 import com.thekr.resources.Res
@@ -68,74 +55,71 @@ fun ZekrBar(
     val localDensity = LocalDensity.current
     var secondaryHeaderHeight by remember { mutableStateOf(0.dp) }
 
+    val sTiny = sTiny
 
-    ConstraintLayout(modifier = modifier) {
-        val (content, trdRow) = createRefs()
-        val sTiny = sTiny
-
-        Column(
-            modifier = Modifier
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(
-                            zekrColors.mainHeaderBackgroundStart,
-                            zekrColors.mainHeaderBackgroundEnd,
-                            zekrColors.mainHeaderBackgroundEnd,
-                        )
+    Column(
+        modifier = Modifier
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        zekrColors.mainHeaderBackgroundStart,
+                        zekrColors.mainHeaderBackgroundEnd,
+                        zekrColors.mainHeaderBackgroundEnd,
                     )
                 )
-                .paint(
-                    painterResource(Res.drawable.header_background),
-                    colorFilter = ColorFilter.tint(zekrColors.mainHeaderBackgroundImageTint),
-                    contentScale = ContentScale.Inside,
-                    repeat = PaintingRepeat.Repeat,
-                )
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            zekrColors.mainHeaderBackgroundEnd.copy(alpha = 0.5f),
-                            zekrColors.mainHeaderBackgroundEnd.copy(alpha = 0.7f),
-                        ),
-                    )
-                )
-                .constrainAs(content) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-        ) {
-            CenterAlignedTopAppBar(
-                navigationIcon = navigationIcon,
-                actions = actions,
-                title = title,
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.Transparent,
-                    navigationIconContentColor = zekrColors.onMainHeader,
-                    titleContentColor = zekrColors.onMainHeader,
-                    actionIconContentColor = zekrColors.onMainHeader
-                ),
             )
-            if (headerTabsRow != null) headerTabsRow()
-        }
-
+            .paint(
+                painterResource(Res.drawable.header_background),
+                colorFilter = ColorFilter.tint(zekrColors.mainHeaderBackgroundImageTint),
+                contentScale = ContentScale.Inside,
+                repeat = PaintingRepeat.Repeat,
+            )
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        zekrColors.mainHeaderBackgroundEnd.copy(alpha = 0.5f),
+                        zekrColors.mainHeaderBackgroundEnd.copy(alpha = 0.7f),
+                    ),
+                )
+            )
+    ) {
+        CenterAlignedTopAppBar(
+            navigationIcon = navigationIcon,
+            actions = actions,
+            title = title,
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.Transparent,
+                navigationIconContentColor = zekrColors.onMainHeader,
+                titleContentColor = zekrColors.onMainHeader,
+                actionIconContentColor = zekrColors.onMainHeader
+            ),
+        )
+        if (headerTabsRow != null) headerTabsRow()
         Box(
-            modifier = Modifier
-                .constrainAs(trdRow) {
-                    top.linkTo(content.bottom, margin = sTiny)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-                .onGloballyPositioned { coordinates ->
-                    secondaryHeaderHeight = with(localDensity) { coordinates.size.height.toDp() }
-                }
-        ) {
-            secondaryHeader?.let { it() }
+            modifier = Modifier.padding(top = sTiny)
+        ){
+            Box(
+                modifier = Modifier
+                    .padding(top = secondaryHeaderHeight /2)
+                    .heightIn(min = secondaryHeaderHeight /2, max = secondaryHeaderHeight /2)
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .onGloballyPositioned { coordinates ->
+                        secondaryHeaderHeight = with(localDensity) { coordinates.size.height.toDp() }
+                    }
+            ) {
+                secondaryHeader?.let { it() }
+            }
         }
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ZekrBarPreviewTemplate(
     azkarState: AzkarState = AzkarState(),
